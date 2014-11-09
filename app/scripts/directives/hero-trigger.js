3c30bd1eb1595acfa40ed94760c429580b3139ad
robinHoover <robin.hoover@gmail.com>
2014-11-09 02:07:52 -0500

    Hero SASS-Maps

---
+++ app/scripts/directives/hero-trigger.js
@@ -0,0 +1,24 @@
+'use strict';
+
+angular
+    .module('ytfApp')
+    .directive('heroTrigger', function ($window) {
+        return {
+            restrict: 'A',
+            scope: {},
+            link: function (scope, element) {
+
+                scope = {};
+                var pages = [' ', 'about', 'contact-form-thank-you', 'contact', 'locate', 'services', 'tall-trees', 'traditions', 'treecare', 'trees'];
+                var url = $window.location.pathname;
+                var file = url.substring(url.lastIndexOf('/')+1);
+                var name = file.split('.');
+
+                var result = getComputedStyle(element[0], ':after').content;
+                result = result.replace(/"/g,''); //Because Firefox keeps quotes from content
+
+                element.addClass(name[0] + '-' + result);
+
+            }
+        };
+    });
